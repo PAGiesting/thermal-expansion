9966f8c49a01fe4795ea05e988b5eea99223179f
Paul G <60189537+PAGiesting@users.noreply.github.com>
2020-08-08 16:21:40 -0400

    first round of plotting, need to pick better / more complete data files

--- collect_data.py
+++ collect_data.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python3
 
-"""Collects processed data files from the th_exp_fit script and moves them from
+"""Collects processed data files from the th_exp_fit script and copies them from
 subdirectories of the data directory into the main directory.
 """
+
+import os
+import shutil
+import json
+import datetime
+
+# Task 1: Identify all processed files with c.txt or x.txt endings.
+# List all subdirectories. Store "leaf" subdirectory names as keys
+# and matching file names in a value list. Skip sapphire standards.
+listing = {}
+for root, dirs, files in os.walk('data'):
+    if dirs != []:
+        continue
+    listing[root]=list(filter(lambda s: (s.endswith('c.csv') | s.endswith('x.csv'))
+        & ('sapph' not in s),files))
+
+# Task 2: Store the current data file listing as a json
+today = datetime.date.today()
+with open('data/'+str(today.toordinal())+'.dat','w') as datfile:
+    json.dump(listing,datfile)
+
+# Task 3: Copy new files up to the main 'data' directory
+for subdir in listing.keys():
+    for filename in listing[subdir]:
+        if os.path.exists('data/'+filename):
+            continue
+        shutil.copy2(subdir+'/'+filename,'data/'+filename)
